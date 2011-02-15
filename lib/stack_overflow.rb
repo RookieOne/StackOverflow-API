ee013cf2c6e2608e4ede36d6f0844f2ec48f17c6
Jonathan Birkholz <rookieone@gmail.com>
2011-02-15 12:54:58 -0600

    moved url to variable

--- lib/stack_overflow.rb
+++ lib/stack_overflow.rb
@@ -3,23 +3,25 @@ require 'httparty'
 class StackOverflow
   include HTTParty
   @@API_KEY = nil
-  
+  @@URL = "http://api.stackoverflow.com/1.0/users/"
+
   def self.API_KEY=(value)
     @@API_KEY = value
   end
 
   def self.get_user(user_id)
-    result = get("http://api.stackoverflow.com/1.0/users/#{user_id}?key=#{@@API_KEY}")
+    result = get(@@URL + "#{user_id}?key=#{@@API_KEY}")
     result["users"].first
   end
   
   def self.get_users(user_ids)
     user_id = user_ids.join(";").to_s
-    result = get("http://api.stackoverflow.com/1.0/users/#{user_id}?key=#{@@API_KEY}")
+    result = get(@@URL + "#{user_id}?key=#{@@API_KEY}")
     result["users"]  
   end
   
   def self.get_user_tags(user_id)
-    get("http://api.stackoverflow.com/1.0/users/#{user_id}/tags?key=#{@@API_KEY}")
+    get(@@URL + "#{user_id}/tags?key=#{@@API_KEY}")
   end
+
 end
